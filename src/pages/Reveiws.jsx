9d63dc62fa55e64a6b8598bfca6eefef292948de
satoshikemboi <satoshikemboi@gmail.com>
2026-03-19 10:48:10 +0300

    minor changes

--- src/pages/Reveiws.jsx
+++ src/pages/Reveiws.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 function Reviews() {
     return (
-        <div className="flex flex-col justify-center py-8 px-32 gap-10">
-        <h1 className="text-5xl leading-tight font-bold text-gray-900">Reveiws</h1>
-        <p className="text-lg text-gray-600">ForexBrokers.com is a website that provides unbiased forex broker reviews and ratings to help individual traders and investors find the best broker for their needs. The company was founded in 2016 and is owned by Reink Media Group, LLC, a privately held Michigan-based company. Each year our team tests and assesses these firms on over 100 variables to formulate star ratings for various categories, as well as the scoring for the best forex brokers.</p>
+        <div className="flex flex-col justify-center py-8 px-32">
+        <h1 className="text-5xl leading-tight font-semibold text-gray-900">Reveiws</h1>
+        <p className="text-lg mt-6 md:font-md text-gray-600">
+            FxBrokers.org is a website that provides unbiased forex broker reviews
+             and ratings to help individual traders and investors find the best broker 
+             for their needs. The company was founded in 2023 and is owned by Tarit Cooperation, 
+             LLC, a privately held Nairobi-based company. Each year our team tests and assesses 
+             these firms on over 100 variables to formulate star ratings for various categories, 
+             as well as the scoring for the best forex brokers.
+        </p>
         </div>
     );
     }
